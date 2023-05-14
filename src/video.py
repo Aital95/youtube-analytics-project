20ea662c1b93ec0ddd94fc583952099a045bd583
user <aytem95@gmail.com>
2023-05-15 02:53:35 +0600

    add_channel_supeer

---
+++ src/video.py
@@ -0,0 +1,40 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    def __init__(self, video_id):
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        video_response = youtube.videos().list(
+            part='snippet,statistics',
+            id=video_id
+        ).execute()
+
+        video_result = video_response.get('items')[0]
+
+        self.id = video_id
+        self.title = video_result['snippet']['title']
+        self.url = f'https://www.youtube.com/watch?v={video_id}'
+        self.view_count = video_result['statistics']['viewCount']
+        self.like_count = video_result['statistics']['likeCount']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        video_response = youtube.videos().list(
+            part='snippet,statistics',
+            id=video_id
+        ).execute()
+
+        video_result = video_response.get('items')[0]
+
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
+
